6138f4a5c43b0db2fd1a18a0ee12c2bf7478fe22
Antonio Paoletti <aptony17@gmail.com>
2018-10-22 15:05:12 -0700

    updates to loging page and databse setup for newLogin

--- CapstoneFinal/CapstoneFinal/index.aspx.cs
+++ CapstoneFinal/CapstoneFinal/index.aspx.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -9,9 +11,47 @@ namespace CapstoneFinal
 {
     public partial class index : System.Web.UI.Page
     {
+        SqlCommand cmd = new SqlCommand();
+        SqlConnection con = new SqlConnection();
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        protected void loginButton_Click(object sender, EventArgs e)
+        {
+            string username = usernameTextBox.Text;
+            string password = passwordTextBox.Text;
+
+            string sql = "SELECT* FROM  newloginTable WHERE username=@username AND [password]=@password";
+
+            // String to store sql connection
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = ConfigurationManager.ConnectionStrings["myDB"].ToString();
+
+
+            SqlCommand sqlCommand = new SqlCommand(sql, con); ;
+            con.Open();
+            cmd.Parameters.AddWithValue("@username", username);
+            cmd.Parameters.AddWithValue("@password", password);
+            cmd.Connection = con;
+            cmd.CommandText = sql;
+            SqlDataReader rd = cmd.ExecuteReader();
+
+            if (rd.HasRows)
+            {
+                while (rd.Read())
+                {
+                    Response.Redirect("index.aspx");
+                }
+            }
+            else
+            {
+                passwordTextBox.Text = "";
+                errorLabel.Visible = true;
+                errorLabel.Text = "Username or password is incorrect!";
+
+            }
         }
     }
 }
