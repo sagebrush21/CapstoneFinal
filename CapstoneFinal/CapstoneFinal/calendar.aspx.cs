6470a945541fd80e3a4aa9afba180be1cc7966ad
sagebrush17 <aptony17@gmail.com>
2018-11-25 03:44:54 -0700

    input and commented code for google api, as it isn't fully funtional yet

--- CapstoneFinal/CapstoneFinal/calendar.aspx.cs
+++ CapstoneFinal/CapstoneFinal/calendar.aspx.cs
@@ -1,17 +1,80 @@
-﻿using System;
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Calendar.v3;
+using Google.Apis.Calendar.v3.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Threading;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Threading.Tasks;
 
 namespace CapstoneFinal
 {
     public partial class calendar : System.Web.UI.Page
     {
+        //static string[] Scopes = { CalendarService.Scope.CalendarReadonly };
+        //static string ApplicationName = "Google Calendar API .NET Quickstart";
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            //UserCredential credential;
+            //var fileName = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "credentials.json");
+           
+            //using (var stream =
+            //    new FileStream(fileName, FileMode.Open, FileAccess.Read))
+            //{
+            //    // The file token.json stores the user's access and refresh tokens, and is created
+            //    // automatically when the authorization flow completes for the first time.
+            //    string credPath = "token.json";
+            //    credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+            //        GoogleClientSecrets.Load(stream).Secrets,
+            //        Scopes,
+            //        "user",
+            //        CancellationToken.None,
+            //        new FileDataStore(credPath, true)).Result;
+            //    Console.WriteLine("Credential file saved to: " + credPath);
+            //}
 
+            //// Create Google Calendar API service.
+            //var service = new CalendarService(new BaseClientService.Initializer()
+            //{
+            //    HttpClientInitializer = credential,
+            //    ApplicationName = ApplicationName,
+            //});
+
+            //// Define parameters of request.
+            //EventsResource.ListRequest request = service.Events.List("primary");
+            //request.TimeMin = DateTime.Now;
+            //request.ShowDeleted = false;
+            //request.SingleEvents = true;
+            //request.MaxResults = 10;
+            //request.OrderBy = EventsResource.ListRequest.OrderByEnum.StartTime;
+
+            //// List events.
+            //Events events = request.Execute();
+            //Console.WriteLine("Upcoming events:");
+            //if (events.Items != null && events.Items.Count > 0)
+            //{
+            //    foreach (var eventItem in events.Items)
+            //    {
+            //        string when = eventItem.Start.DateTime.ToString();
+            //        if (String.IsNullOrEmpty(when))
+            //        {
+            //            when = eventItem.Start.Date;
+            //        }
+            //        Console.WriteLine("{0} ({1})", eventItem.Summary, when);
+            //    }
+            //}
+            //else
+            //{
+            //    Console.WriteLine("No upcoming events found.");
+            //}
+            //Console.Read();
         }
     }
 }
